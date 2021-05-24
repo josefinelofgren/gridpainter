1b06026ea39d6cb9e8b781f69104a3411f6e8283
Josefine Löfgren <josefinelofgren@Josefines-MacBook-Pro.local>
2021-05-24 09:53:20 +0200

    small changes in game.html

--- public/javascripts/modules/play.mjs
+++ public/javascripts/modules/play.mjs
@@ -3,35 +3,6 @@ import { randomPics } from '../modules/array.mjs';
 import { printImage, saveDrawnPic, savedPic } from '../modules/paint.mjs';
 
 const socket = io();
-
-// PLAY GAME
-socket.emit('playGame', {username, color});
-
-// MESSAGE FROM SERVER
-socket.on('ready', message => {
-    outputPressPlay(message)
-});
-
-// OUTPUT USER IF USER PRESS PLAY 
-function outputPressPlay(message){
-
-    let usersReadyToPlay = document.createElement('ul');
-    usersReadyToPlay.setAttribute("id", "usersReadyToPlay");
-
-    document.getElementById('gameInfo').appendChild(usersReadyToPlay);
-
-    usersReadyToPlay.innerHTML += `<li>${message.text}</li>`
-};
-
-// INPUT USER IF USER PRESS PLAY 
-function inputPressPlay(){
-    
-    // Emit message to server
-    socket.emit('play');
-
-    document.getElementById('playBtn').innerHTML = `<i class="fa-spin fas fa-spinner"></i>Waiting for the other players..`
-};
-
 const facitGrid = document.querySelector('#facit');
 
 
@@ -113,6 +84,35 @@ function compare(randomPic, savedPic) {
   );
 };
 
+
+
+// PLAY GAME
+socket.emit('playGame', {username, color});
+
+// MESSAGE FROM SERVER
+socket.on('ready', message => {
+    outputPressPlay(message)
+});
+
+// OUTPUT USER IF USER PRESS PLAY 
+function outputPressPlay(message){
+
+    let usersReadyToPlay = document.createElement('ul');
+    usersReadyToPlay.setAttribute("id", "usersReadyToPlay");
+
+    document.getElementById('gameInfo').appendChild(usersReadyToPlay);
+
+    usersReadyToPlay.innerHTML += `<li>${message.text}</li>`
+};
+
+// INPUT USER IF USER PRESS PLAY 
+function inputPressPlay(){
+    
+    // Emit message to server
+    socket.emit('play');
+
+    document.getElementById('playBtn').innerHTML = `<i class="fa-spin fas fa-spinner"></i>Waiting for the other players..`
+};
 
 
 export { findRandomPic, playBtnAction, compare, inputPressPlay};
